45a971bd0bc7aba91531079468aee55771cd8149
Alexandru Radovici <msg4alex@gmail.com>
2014-07-17 22:58:38 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if [ $wyliodrin_board == "raspberrypi" ]; then
+if [ "$wyliodrin_board" == "raspberrypi" ]; then
 	cp lib/hardfp/*.so /usr/local/lib
 fi
 
-if [ $wyliodrin_board == "arduinogalileo" ]; then
+if [ "$wyliodrin_board" == "arduinogalileo" ]; then
 	cp lib/x86/*.so /usr/local/lib
 fi
 
